b7c4764f10a8d6809a0b1ef477ba588b6e6eff70
Manu-Andaluz <manupeandaluz@gmail.com>
2023-12-10 21:38:19 -0300

    fixing vercel errors

--- backend/account/views.py
+++ backend/account/views.py
@@ -3,13 +3,12 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 # from .models import User
 from .serializers import UserRegistrationSerializer
-from rest_framework.permissions import AllowAny 
 from django.http import JsonResponse
 # Create your views here.
 
 class UserRegistrationViewSet(viewsets.ViewSet):
     serializer_class = UserRegistrationSerializer
-    permission_classes = [AllowAny]
+
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
