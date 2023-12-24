85b591e2567b19aa4de2e7aac2dafa4cf1e055d4
Manu-Andaluz <manupeandaluz@gmail.com>
2023-12-24 18:30:46 -0300

    adding railway path

--- frontend/src/components/PostDetails/hooks/DELETE/deletePost.ts
+++ frontend/src/components/PostDetails/hooks/DELETE/deletePost.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const deletePost = async (postId: any) => {
-  const apiUrl = `http://localhost:8000/post/all_posts/${postId}`; // Replace with your API endpoint
+  const apiUrl = `https://thoughts-production.up.railway.app/post/all_posts/${postId}`; // Replace with your API endpoint
 
   const res = await axios.delete(apiUrl);
   console.log("res : ", res);
