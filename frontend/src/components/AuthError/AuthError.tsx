64d090094dba4a64d4bc56a15005ee71135e6569
Manu-Andaluz <manupeandaluz@gmail.com>
2024-01-09 03:38:24 -0300

    adding new icon for auth error default page

--- frontend/src/components/AuthError/AuthError.tsx
+++ frontend/src/components/AuthError/AuthError.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import style from "./AuthError.module.scss";
-import { WarningIcon } from "./icons";
+import Image from "next/image";
 
 const AuthError = () => {
   return (
     <div className={style.container}>
-      <WarningIcon />
-      <h1>You must be a user to access to this feature !!</h1>
+      <Image src={"/join.svg"} width={600} height={600} alt="info.svg" />
+      <h1>You must be a user to get access to this feature !!</h1>
     </div>
   );
 };
